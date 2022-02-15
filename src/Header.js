eb879ab8e512d703a651af32a1e47b53dc115c1a
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-15 15:41:52 -0800

    finished lab -02

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import './Header.css';
+
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      className: 'h1'
+    }
+  }
+
+  classNameChange = () => {
+    if (this.state.className === 'h1') {
+      this.setState({className: 'h1Crimson'});
+    } else {
+      this.setState({className: 'h1'})
+    }
+  }
+
   render() {
     return (
       <header>
-        <h1>Gallery of Horns</h1>
+        <h1 className={this.state.className} onClick={this.classNameChange}>Gallery of Horns</h1>
       </header>
     )
   }
