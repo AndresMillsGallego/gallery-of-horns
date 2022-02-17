112f17ac76319225cd3e32b8bdbb88ea1c53d09f
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-16 16:35:18 -0800

    finished lab 03

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import data from './data.json'
+import React from 'react';
+import SelectedBeast from './SelectedBeast';
 
-function App() {
-  return (
-    <>
-    <Header />
-    <Main data={data}/>
-    <Footer />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayModal: true,
+      beast: {}
+    }
+  }
+
+  closeModal = () => this.setState({displayModal: false});
+
+  showSelectedBeast = (beast) => {
+    this.setState({ 
+      displayModal: true,
+      beast
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <SelectedBeast 
+          displayModal={this.state.displayModal}
+          closeModal={this.closeModal}
+          beast={this.state.beast}
+          />
+        <Header />
+        <Main 
+          data={data} 
+          showSelectedBeast={this.showSelectedBeast} 
+          />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/SelectedBeast.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+
+import './SelectedBeast.css'
+
+class SelectedBeast extends React.Component {
+
+
+  render() {
+    return (
+      <Modal 
+        show={this.props.displayModal} 
+        size='lg'
+        centered
+        onHide={this.props.closeModal}
+        
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.beast.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.props.beast.image_url} alt={this.props.beast.title} title={this.props.beast.title} style={{width:'100%'}}></img>
+          </Modal.Body>
+          <Modal.Footer>{this.props.beast.description}</Modal.Footer>
+        </Modal>
+    )
+  }
+}
+
+export default SelectedBeast;
